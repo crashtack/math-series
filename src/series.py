f72e22a62cdef00a506a2da2445e305c11e6eece
crashtack <crashtack@gmail.com>
2016-08-09 14:34:16 -0700

    added a doc string and an __name__ = __main__: block

--- src/series.py
+++ src/series.py
@@ -1,3 +1,8 @@
+"""
+    This module definces functions that implement mathematical series
+"""
+
+
 def fib(n):
     if n == 0:
         return 0
@@ -20,3 +25,24 @@ def generic_sequence(n, s):
     elif n == 1:
         return 1
     return generic_sequence(n - 1, s) + generic_sequence(n - 2, s)
+
+if __name__ == '__main__':
+    print()
+    print(u'This module definces functions that implement mathematical series.')
+    print()
+    print(u'fibonacci(n):')
+    print(u'\tReturns the nth value in the fibonacci series.')
+    print()
+    print(u'>>> fibonacci(3) returns: {}'.format(fib(3)))
+    print()
+    print(u'lucas(n):')
+    print(u'\tReturns the nth value in the lucas series.')
+    print()
+    print(u'>>> lucas(3) returns: {}'.format(lucas(3)))
+    print()
+    print(u'generic_sequence(n):')
+    print(u'\tReturns the nth value in the series based on the entered seed value.')
+    print()
+    print(u'>>> generic_sequence(3, 2) returns: {}'.format(generic_sequence(3,2)))
+    print(u'which is the same as the lucas series')
+    print()
